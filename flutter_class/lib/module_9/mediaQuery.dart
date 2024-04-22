a279c6dd00a8ac3df2658dca8787f5d2fcc5f244
mahmud5658 <almahmuda5@gmail.com>
2024-04-22 21:51:20 +0600

    flutter class

---
+++ flutter_class/lib/module_9/mediaQuery.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Media extends StatelessWidget {
+  const Media({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size);
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).orientation);
+    print(MediaQuery.of(context).devicePixelRatio);
+    print(MediaQuery.of(context).displayFeatures);
+
+    print(MediaQuery.displayFeaturesOf(context));
+    print(MediaQuery.sizeOf(context));
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Media Query'),
+          backgroundColor: Theme.of(context).primaryColor),
+          body: Center(
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.end,
+              spacing: 4,
+              children: [
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+                Text(MediaQuery.of(context).orientation.toString()),
+              ],
+            ),
+          ),
+    );
+  }
+}
