445a47b53818993109f32851cc5b15295c792de7
mahmud5658 <almahmuda5@gmail.com>
2024-04-30 23:46:55 +0600

    Live_test

--- module_10/live_test/lib/home.dart
+++ module_10/live_test/lib/home.dart
@@ -14,45 +14,47 @@ class HomePage extends StatelessWidget {
         body: OrientationBuilder(
           builder: (context, orientation) {
             if (orientation == Orientation.portrait) {
-              return Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 30),
-                      height: MediaQuery.of(context).size.height * .20,
-                      width: MediaQuery.of(context).size.width * .40,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.blue,
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  'https://assets.goal.com/images/v3/blt6e29869bfab4c3a8/Neymar.jpg?auto=webp&format=pjpg&width=1080&quality=60'))),
-                    ),
-                    Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        child: const Text(
-                          'Neymar JR',
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        )),
-                    Container(
-                        margin: const EdgeInsets.only(top: 5),
-                        child: const Text(
-                          'neymarjonior@gmail.com',
-                          style: TextStyle(color: Colors.grey, fontSize: 20),
-                        )),
-                    Container(
-                        margin:
-                            const EdgeInsets.only(top: 15, right: 20, left: 20),
-                        child: const Center(
-                          child: Text(
-                            textAlign: TextAlign.center,
-                            'Neymar da Silva Santos Júnior, commonly known as Neymar Jr., is a Brazilian professional footballer widely regarded as one of the most talented players of his generation. Born on February 5, 1992, in Mogi das Cruzes, São Paulo, Brazil, Neymar began his football career at a young age and quickly rose through the ranks to become a star player.',
+              return SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 30),
+                        height: MediaQuery.of(context).size.height * .20,
+                        width: MediaQuery.of(context).size.width * .40,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    'https://assets.goal.com/images/v3/blt6e29869bfab4c3a8/Neymar.jpg?auto=webp&format=pjpg&width=1080&quality=60'))),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          child: const Text(
+                            'Neymar JR',
                             style: TextStyle(color: Colors.black, fontSize: 20),
-                          ),
-                        )),
-                  ],
+                          )),
+                      Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          child: const Text(
+                            'neymarjonior@gmail.com',
+                            style: TextStyle(color: Colors.grey, fontSize: 20),
+                          )),
+                      Container(
+                          margin:
+                              const EdgeInsets.only(top: 15, right: 20, left: 20),
+                          child: const Center(
+                            child: Text(
+                              textAlign: TextAlign.center,
+                              'Neymar da Silva Santos Júnior, commonly known as Neymar Jr., is a Brazilian professional footballer widely regarded as one of the most talented players of his generation. Born on February 5, 1992, in Mogi das Cruzes, São Paulo, Brazil, Neymar began his football career at a young age and quickly rose through the ranks to become a star player.',
+                              style: TextStyle(color: Colors.black, fontSize: 20),
+                            ),
+                          )),
+                    ],
+                  ),
                 ),
               );
             } else {
